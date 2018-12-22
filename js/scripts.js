e95c442c4d78261270797131b6d9e3a23ec167c2
Senn Pateet <senn.patteet@hotmail.com>
2018-12-23 00:30:40 +0100

    layout, jquery and pauline test

---
+++ js/scripts.js
@@ -0,0 +1,217 @@
+$( document ).ready(function() {
+$('.button1').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg2').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button1up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg').offset().top
+    }, 500);
+});
+
+// -------------------------------------------------------------------
+
+});
+
+$( document ).ready(function() {
+$('.button2').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg3').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button2up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg2').offset().top
+    }, 500);
+});
+
+});
+
+// -------------------------------------------------------------------
+
+$( document ).ready(function() {
+$('.button3').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg4').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button3up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg3').offset().top
+    }, 500);
+});
+
+});
+
+// -------------------------------------------------------------------
+
+$( document ).ready(function() {
+$('.button4').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg5').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button4up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg4').offset().top
+    }, 500);
+});
+
+});
+
+// -------------------------------------------------------------------
+
+$( document ).ready(function() {
+$('.button5').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg6').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button5up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg5').offset().top
+    }, 500);
+});
+
+});
+
+// -------------------------------------------------------------------
+
+$( document ).ready(function() {
+$('.button6').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg7').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button6up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg6').offset().top
+    }, 500);
+});
+
+});
+
+// -------------------------------------------------------------------
+
+$( document ).ready(function() {
+$('.button7').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg8').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button7up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg7').offset().top
+    }, 500);
+});
+
+});
+
+// -------------------------------------------------------------------
+
+$( document ).ready(function() {
+$('.button8').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg9').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button8up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg8').offset().top
+    }, 500);
+});
+
+});
+
+// -------------------------------------------------------------------
+
+$( document ).ready(function() {
+$('.button9').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg10').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button9up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg9').offset().top
+    }, 500);
+});
+
+});
+
+// -------------------------------------------------------------------
+
+$( document ).ready(function() {
+$('.button10').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg11').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button10up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg10').offset().top
+    }, 500);
+});
+
+});
+
+// -------------------------------------------------------------------
+
+$( document ).ready(function() {
+$('.button11').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg12').offset().top
+    }, 500);
+});
+
+});
+
+$( document ).ready(function() {
+$('.button11up').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('.bg11').offset().top
+    }, 500);
+});
+
+});
